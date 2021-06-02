dae97381aa8841b64e4db28f036f5271da80682d
Miguel <miguelarrieta02@gmail.com>
2021-06-02 18:40:58 +0100

    Added two stack implementations

---
+++ src/com/chuset/stacks/usingArray/MyStack.java
@@ -0,0 +1,67 @@
+package com.chuset.stacks.usingArray;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+public class MyStack<E> {
+
+    private final List<E> dynamicArray;
+
+    // Default size of 10
+    public MyStack() {
+        dynamicArray = new ArrayList<>();
+    }
+
+    public MyStack(final int size) {
+        dynamicArray = new ArrayList<>(size);
+    }
+
+    public void push(final E value) {
+        dynamicArray.add(value);
+    }
+
+    public E pop() {
+        if (dynamicArray.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return dynamicArray.remove(dynamicArray.size() - 1);
+    }
+
+    public E peek() {
+        if (dynamicArray.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return dynamicArray.get(dynamicArray.size() - 1);
+    }
+
+    @Override
+    public String toString() {
+        if (dynamicArray.isEmpty()) {
+            return "";
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        final int limit = dynamicArray.size() - 1;
+        for (int i = 0; i < limit; i++) {
+            sb.append(i + 1).append(": ").append(dynamicArray.get(i)).append('\n');
+        }
+        return sb.append(dynamicArray.size()).append(": ").append(dynamicArray.get(dynamicArray.size() - 1)).toString();
+    }
+
+    public static void main(String[] args) {
+        final MyStack<Integer> stack = new MyStack<>();
+        stack.push(5);
+        System.out.printf("stack = %n%s%n", stack);
+
+        stack.push(3);
+        System.out.printf("stack = %n%s%n", stack);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.printf("stack = %n%s%n", stack);
+
+        stack.pop();
+        System.out.printf("stack = %n%s%n", stack);
+    }
+}
---
+++ src/com/chuset/stacks/usingLinkedList/MyStack.java
@@ -0,0 +1,83 @@
+package com.chuset.stacks.usingLinkedList;
+
+import com.chuset.linkedLists.singly.MySinglyLinkedNode;
+
+import java.util.EmptyStackException;
+
+public class MyStack<E> {
+
+    private MySinglyLinkedNode<E> top;
+//    private MySinglyLinkedNode<E> bottom;
+    private int length;
+
+    public MyStack() {
+        top = null;
+//        bottom = null;
+        length = 0;
+    }
+
+    public void push(final E value) {
+        top = new MySinglyLinkedNode<>(value, top);
+//        if (length == 0) {
+//            bottom = top;
+//        }
+        length++;
+    }
+
+    public E pop() {
+        if (length == 0) {
+            throw new EmptyStackException();
+        }
+
+        final MySinglyLinkedNode<E> poppedValue = top;
+        top = top.getNext();
+        length--;
+
+//        if (length == 0) {
+//            bottom = null;
+//        }
+
+        return poppedValue.getValue();
+    }
+
+    public E peek() {
+        if (length == 0) {
+            throw new EmptyStackException();
+        }
+
+        return top.getValue();
+    }
+
+    @Override
+    public String toString() {
+        MySinglyLinkedNode<E> currentNode = top;
+        final StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; currentNode != null ; i++) {
+            sb.append(i).append(": ").append(currentNode.getValue());
+            final MySinglyLinkedNode<E> temp = currentNode.getNext();
+
+            if (temp != null) {
+                sb.append('\n');
+            }
+            currentNode = temp;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        final MyStack<Integer> stack = new MyStack<>();
+        stack.push(5);
+        System.out.printf("stack = %n%s%n", stack);
+
+        stack.push(3);
+        System.out.printf("stack = %n%s%n", stack);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.printf("stack = %n%s%n", stack);
+
+        stack.pop();
+        System.out.printf("stack = %n%s%n", stack);
+    }
+}
