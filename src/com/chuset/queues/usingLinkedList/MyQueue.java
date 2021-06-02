a23bd9e6a9bcbfebb5c16b8d7c6f3494d93c5bd4
Miguel <miguelarrieta02@gmail.com>
2021-06-02 19:40:18 +0100

    Added two queue implementations

--- src/com/chuset/queues/usingLinkedList/MyQueue.java
+++ src/com/chuset/queues/usingLinkedList/MyQueue.java
@@ -1,2 +1,89 @@
-package com.chuset.queues.usingLinkedList;public class MyQueue {
+package com.chuset.queues.usingLinkedList;
+
+import com.chuset.linkedLists.singly.MySinglyLinkedNode;
+
+public class MyQueue<E> {
+
+    private MySinglyLinkedNode<E> first;
+    private MySinglyLinkedNode<E> last;
+    private int length;
+
+    public MyQueue() {
+        first = null;
+        last = null;
+        length = 0;
+    }
+
+    public void enqueue(final E value) {
+        final MySinglyLinkedNode<E> newNode = new MySinglyLinkedNode<>(value);
+        if (length == 0) {
+            first = newNode;
+        } else {
+            last.setNext(newNode);
+        }
+        last = newNode;
+        length++;
+    }
+
+    public E dequeue() {
+        if (first == null) {
+            throw new EmptyQueueException();
+        }
+
+        final MySinglyLinkedNode<E> result = first;
+        first = first.getNext();
+        length--;
+
+        if (length == 0) {
+            last = null;
+        }
+
+        return result.getValue();
+    }
+
+    public E peek() {
+        if (first == null) {
+            throw new EmptyQueueException();
+        }
+        return first.getValue();
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    @Override
+    public String toString() {
+        MySinglyLinkedNode<E> currentNode = first;
+        final StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; currentNode != null; i++) {
+            sb.append(i).append(": ").append(currentNode.getValue());
+            final MySinglyLinkedNode<E> temp = currentNode.getNext();
+
+            if (temp != null) {
+                sb.append('\n');
+            }
+            currentNode = temp;
+        }
+        return sb.toString();
+    }
+
+    private static class EmptyQueueException extends RuntimeException {
+    }
+
+    public static void main(String[] args) {
+        final MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("John");
+        queue.enqueue("Cassidy");
+        queue.enqueue("David");
+        System.out.printf("queue = %n%s%n", queue);
+
+        System.out.printf("Dequeued value: %s%n", queue.dequeue());
+        System.out.printf("queue = %n%s%n", queue);
+
+        System.out.printf("Peeked value: %s%n", queue.peek());
+        System.out.printf("Dequeued value: %s%n", queue.dequeue());
+        System.out.printf("queue = %n%s%n", queue);
+    }
 }
--- src/com/chuset/queues/usingStack/MyQueue.java
+++ src/com/chuset/queues/usingStack/MyQueue.java
@@ -1,2 +1,71 @@
-package com.chuset.queues.usingStack;public class MyQueue {
+package com.chuset.queues.usingStack;
+
+import java.util.Stack;
+
+public class MyQueue<E> {
+
+    private final Stack<E> stack;
+    private final Stack<E> auxiliaryStack;
+
+    public MyQueue() {
+        stack = new Stack<>();
+        auxiliaryStack = new Stack<>();
+    }
+
+    public void enqueue(final E value) {
+        stack.push(value);
+    }
+
+    public E dequeue() {
+        fillAuxiliaryStackWithStack();
+        final E value = auxiliaryStack.pop();
+        fillStackWithAuxiliaryStack();
+        return value;
+    }
+
+    public E peek() {
+        fillAuxiliaryStackWithStack();
+        final E value = auxiliaryStack.peek();
+        fillStackWithAuxiliaryStack();
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        final Object[] stackAsArray = stack.toArray();
+        final StringBuilder sb = new StringBuilder();
+
+        final int limit = stackAsArray.length - 1;
+        for (int index = 0; index < limit; index++) {
+            sb.append(index + 1).append(": ").append(stackAsArray[index]).append('\n');
+        }
+        return sb.append(stackAsArray.length).append(": ").append(stackAsArray[limit]).toString();
+    }
+
+    private void fillAuxiliaryStackWithStack() {
+        while (!stack.isEmpty()) { // Store the reverse order of the stack in "auxiliaryStack"
+            auxiliaryStack.push(stack.pop());
+        }
+    }
+
+    private void fillStackWithAuxiliaryStack() {
+        while (!auxiliaryStack.isEmpty()) { // Return stack to the original state
+            stack.push(auxiliaryStack.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        final MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("John");
+        queue.enqueue("Cassidy");
+        queue.enqueue("David");
+        System.out.printf("queue = %n%s%n", queue);
+
+        System.out.printf("Dequeued value: %s%n", queue.dequeue());
+        System.out.printf("queue = %n%s%n", queue);
+
+        System.out.printf("Peeked value: %s%n", queue.peek());
+        System.out.printf("Dequeued value: %s%n", queue.dequeue());
+        System.out.printf("queue = %n%s%n", queue);
+    }
 }
