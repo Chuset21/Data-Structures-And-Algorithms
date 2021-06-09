852bfc7ffe4443573e8df2c2d0d6c70e485d1721
Miguel <miguelarrieta02@gmail.com>
2021-06-09 16:28:58 +0100

    Added method to insert words from a CSV file

--- src/com/chuset/data_structures/trees/trie/MyTrie.java
+++ src/com/chuset/data_structures/trees/trie/MyTrie.java
@@ -71,6 +71,32 @@ public class MyTrie {
         }
     }
 
+    public void insertFromCSV(final File file) throws IOException {
+        // This is a bit slower but much easier to read
+//        new BufferedReader(new FileReader(file)).lines().map(row -> StringTrimmer.trim(row.split(","))).
+//                forEach(this::insertArray);
+        final BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String row;
+        while ((row = bufferedReader.readLine()) != null) {
+            this.insertArray(StringTrimmer.trim(row.split(",")));
+        }
+    }
+
+    public void insertArray(final String[] strings) {
+        for (final String s : strings) {
+            this.insert(s);
+        }
+    }
+
+    private static class StringTrimmer {
+        private static String[] trim(final String[] strings) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].trim();
+            }
+            return strings;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         final MyTrie dictionary = new MyTrie();
         System.out.printf("The dictionary is%s empty.%n", dictionary.isEmpty() ? "" : "n't");
